2eb75dfd3d28c9a7b80983368b84f7ce919078d4
Vika2335 <golobokova.viktoriakpc602020@gmail.com>
2024-03-26 10:53:48 +0300

    refactor: changed massiv

--- src/page/Catalog/Catalog.jsx
+++ src/page/Catalog/Catalog.jsx
@@ -4,14 +4,6 @@ import AddCard from '../../components/AddCard/AddCard.jsx';
 import Popup from '../../components/Popup/Popup.jsx';
 import './Catalog.css';
 import { data } from '../../utils/data.js'
-import katrine from '../../images/pets/pets-katrine.png';
-import jennifer from '../../images/pets/pets-jennifer.png';
-import woody from '../../images/pets/pets-woody.png';
-import sophie from '../../images/pets/pets-sophia.png';
-import timmy from '../../images/pets/pets-timmy.png';
-import charly from '../../images/pets/pets-charly.png';
-import scarlett from '../../images/pets/pets-scarlet.png';
-import freddie from '../../images/pets/pets-freddie.png';
 
 function generateUniqueId() {
   return Math.random().toString();
@@ -21,16 +13,7 @@ function Catalog() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedPet, setSelectedPet] = useState(null);
 
-  const [pets, setPets] = useState([
-    { id: '5', imgSrc: katrine, name: 'Katrine' },
-    { id: '1', imgSrc: jennifer, name: 'Jennifer' },
-    { id: '3', imgSrc: woody, name: 'Woody' },
-    { id: '2', imgSrc: sophie, name: 'Sophia' },
-    { id: '6', imgSrc: timmy, name: 'Timmy' },
-    { id: '8', imgSrc: charly, name: 'Charly' },
-    { id: '4', imgSrc: scarlett, name: 'Scarlett' },
-    { id: '7', imgSrc: freddie, name: 'Freddie' },
-  ]);
+  const [pets, setPets] = useState(data);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -69,7 +52,7 @@ function Catalog() {
               <h1 className="pets-content__heading">Our friends who are looking for a house</h1>
               <div className="catalog">
                 {pets.map(pet => (
-                  <Card key={pet.id} id={pet.id} imgSrc={pet.imgSrc} name={pet.name} onDelete={() => deleteCard(pet.id)} setSelectedPet={setSelectedPet}/>
+                  <Card key={pet.id} id={pet.id} imgSrc={pet.img} name={pet.name} onDelete={() => deleteCard(pet.id)} setSelectedPet={setSelectedPet}/>
                 ))}
               </div>
               <div className='addCard'>
